d605c0f73aed37817f8ff52b33413ee8b57c1eb9
Jacob Murphy <mrjacobmurphy@gmail.com>
2020-02-20 11:30:57 -0500

    footer for later

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import PropTypes from "prop-types"
 import github from "../images/GitHub-Mark-120px-plus.png"
 
 const Footer = class extends React.Component {
   render() {
-    return <footer></footer>
+    return (
+      <footer>
+        <div className="row">
+          <div class="col-sm">
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </div>
+          <div class="col-sm">
+            <a title="github" href="https://github.com/jacobhmurphy">
+              <img src={github} alt="GitHub" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    )
   }
 }
+
+Footer.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Footer
