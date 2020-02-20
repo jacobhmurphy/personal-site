6cba220c9a6c2d94c34e4b2b1c2135af2ab0d269
Jacob Murphy <mrjacobmurphy@gmail.com>
2020-02-20 02:03:20 -0500

    updating index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,34 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            nesciunt quidem voluptate saepe, illum inventore est quos.
+            Perspiciatis architecto, eaque iusto modi laboriosam quae
+            aspernatur, molestiae distinctio impedit nihil reprehenderit.
+          </p>
+        </div>
+
+        <div className="col-md">
+          <h1>Blog</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+            asperiores corporis nulla voluptatem, libero, repellat voluptates
+            praesentium corrupti quidem dolores nemo sunt. Labore dolores neque
+            perspiciatis vero deleniti totam tempore?
+          </p>
+        </div>
+      </div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
