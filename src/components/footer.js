e9cab0a5056b4676a81c6aa026b9e9d6689666cd
Jacob Murphy <mrjacobmurphy@gmail.com>
2020-02-20 02:02:29 -0500

    creating footer component

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+import github from "../images/GitHub-Mark-120px-plus.png"
+
+const Footer = class extends React.Component {
+  render() {
+    return <footer></footer>
+  }
+}
