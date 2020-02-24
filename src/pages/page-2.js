8ba200b06543fc692bcb2acb202abc8904be67f9
Jacob Murphy <mrjacobmurphy@gmail.com>
2020-02-23 22:48:05 -0500

    test edit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,7 +11,6 @@ const SecondPage = () => (
     <div className="container-fluid">
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
     </div>
   </Layout>
 )
