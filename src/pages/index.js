a271512dbc57f4f55e27165896e39b34dd5b7444
Jacob Murphy <mrjacobmurphy@gmail.com>
2020-02-23 22:47:56 -0500

    editing text on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import StyledHomepageHero from "../components/backgroundsection"
@@ -15,25 +14,12 @@ const IndexPage = () => (
         <div className="col-md">
           <h1>About</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
-            nesciunt quidem voluptate saepe, illum inventore est quos.
-            Perspiciatis architecto, eaque iusto modi laboriosam quae
-            aspernatur, molestiae distinctio impedit nihil reprehenderit.
-          </p>
-        </div>
-
-        <div className="col-md">
-          <h1>Blog</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
-            asperiores corporis nulla voluptatem, libero, repellat voluptates
-            praesentium corrupti quidem dolores nemo sunt. Labore dolores neque
-            perspiciatis vero deleniti totam tempore?
+            My name is Jacob Murphy, and I'm a software developer, specializing
+            in web development. My GitHub profile can be found in the navbar.
           </p>
         </div>
       </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
